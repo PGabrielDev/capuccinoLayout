3f646153d1c36b9c11fcb58fe5de7dfbe811b5ab
paulo gabriel <pgabrieldeveloper@outlook.com>
2023-09-25 15:40:12 -0300

    initiaal commit

---
+++ app/src/main/java/curso/kotlin/capuccino/MainActivity.kt
@@ -0,0 +1,11 @@
+package curso.kotlin.capuccino
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
